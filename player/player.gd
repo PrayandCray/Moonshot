812f75a8560cc1a184c45eca0ebbd1061cda8fe8
PrayandCray <104704414+PrayandCray@users.noreply.github.com>
2025-10-28 02:19:01 -0400

    got basic movement up and running

---
+++ player/player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody3D
+
+@onready var head: Node3D = $Head
+
+const SENSITIVITY = 0.003
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event) -> void:
+	if event is InputEventMouseMotion:
+		rotate_object_local(Vector3.UP, -event.relative.x * SENSITIVITY)
+		head.rotate_x(-event.relative.y * SENSITIVITY)
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-40), deg_to_rad(60)) # clamp to not break ur neck lol
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
